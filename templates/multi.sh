c89211507ed76e9313a45a71f1594c341169d11e
Sam Minot <sminot@fredhutch.org>
2022-09-01 10:27:58 -0700

    Disable UI

--- templates/multi.sh
+++ templates/multi.sh
@@ -9,5 +9,6 @@ SAMPLE="\${CSV%.csv}"
 cellranger multi \
            --id=\$SAMPLE \
            --csv=\$CSV \
+           --noui \
            --localcores=${task.cpus} \
            --localmem=${task.memory.toGiga()}
