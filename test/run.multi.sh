7b65b3f9d82f740f48bc08c8a2f3c4895dea81d9
Sam Minot <sminot@fredhutch.org>
2022-09-01 10:14:22 -0700

    Fix path to inputs

--- test/run.multi.sh
+++ test/run.multi.sh
@@ -11,7 +11,7 @@ run_cellranger_multi(){
         ../${TOOL}.nf \
         --output run/${TOOL} \
         --grouping multi.grouping.csv \
-        --fastq_dir data/multi/pbmc_1k_v3_fastqs \
+        --fastq_dir data/multi/fastqs \
         --transcriptome_dir data/ref/refdata-gex-GRCh38-2020-A \
         --vdj_dir data/ref/refdata-cellranger-vdj-GRCh38-alts-ensembl-5.0.0 \
         -resume \
