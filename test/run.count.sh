75ee3d5462f95ba368c7cffcc3230f268ba3c1c2
Sam Minot <sminot@fredhutch.org>
2022-10-07 12:42:37 -0700

    Add tests for the version and include_introns flags

--- test/run.count.sh
+++ test/run.count.sh
@@ -26,6 +26,25 @@ run_cellranger_count(){
         -resume \
         $1
     
+    # Test two different versions
+    for VER in 6.1.1 7.0.1; do
+
+        # Test with an without introns
+        for INTRON in true false; do
+            nextflow \
+                run \
+                ../${TOOL}.nf \
+                --output run/${TOOL}/$VER/include_introns_$INTRON \
+                --fastq_dir data/count/pbmc_1k_v3_fastqs \
+                --transcriptome_dir data/ref/refdata-gex-GRCh38-2020-A \
+                --cellranger_version $VER \
+                --include_introns $INTRON \
+                -resume \
+                $1
+
+        done
+    done
+    
 }
 
 run_cellranger_count $1
