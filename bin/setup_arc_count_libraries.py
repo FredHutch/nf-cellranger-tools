853578ab01b258a5cf3a0eeea10f0c951f85d382
Sam Minot <sminot@fredhutch.org>
2023-11-17 08:47:44 -0800

    drop_duplicates

--- bin/setup_arc_count_libraries.py
+++ bin/setup_arc_count_libraries.py
@@ -52,5 +52,6 @@ def remove_end_options(file_name: str, endings: List[str]) -> str:
         for row in parse_samples(folder, library_type)
     ])
     .reindex(columns=["fastqs", "sample", "library_type"])
+    .drop_duplicates()
     .to_csv("libraries.csv", index=False)
 )
